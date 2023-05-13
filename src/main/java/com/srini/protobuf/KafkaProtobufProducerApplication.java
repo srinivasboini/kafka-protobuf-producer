fdde087ab237d41870ab7a0f4dff7daee659141f
Srini <srinivasboini7@gmail.com>
2023-05-13 16:52:28 +0800

    Added proto files

--- src/main/java/com/srini/protobuf/KafkaProtobufProducerApplication.java
+++ src/main/java/com/srini/protobuf/KafkaProtobufProducerApplication.java
@@ -1,16 +1,23 @@
 package com.srini.protobuf;
 
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaProtobufProducerApplication {
+@RequiredArgsConstructor
+public class KafkaProtobufProducerApplication implements CommandLineRunner {
 
-
+	private final Producer producer ;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaProtobufProducerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		producer.send();
+	}
 }
--- src/main/java/com/srini/protobuf/Producer.java
+++ src/main/java/com/srini/protobuf/Producer.java
@@ -6,19 +6,32 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @Component
 @RequiredArgsConstructor
 public class Producer {
 
-    private final KafkaTemplate<String, StockProtos> kafkaTemplate ;
+    private final KafkaTemplate<String, StockProtos.Stock> kafkaTemplate ;
 
     public void send(){
-        /*IntStream.rangeClosed(1,500)
-                .mapToObj(i -> invoiceGenerator.getNextInvoice())
-                .map(invoice -> new ProducerRecord<>("invoice-topic-1", invoice.getStoreID().toString(), invoice))
+        getStocks().stream()
+                .map(e -> new ProducerRecord<>("stock-topic-1", e.getName(), e))
                 .forEach(kafkaTemplate::send) ;
-*/
+    }
+
+    List<StockProtos.Stock> getStocks(){
+        List<StockProtos.Stock> stocks = new ArrayList<>() ;
+        StockProtos.Stock stock = StockProtos.Stock.newBuilder()
+                .setName("ICICI")
+                .setVolume(20)
+                .setIsin(200)
+                .build() ;
+
+        stocks.add(stock) ;
+        return  stocks ;
+
     }
 }
